b1de0435779c0bbee791dde84c3c314b93cda33d
Sebastian Osiński <seb.osinski@gmail.com>
2015-12-03 15:54:13 +0100

    Initial Commit

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Sebastian Osiński on 03.12.2015.
+//  Copyright © 2015 Sebastian Osiński. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
