d327e9688524e36590aca770deb9538b310dbc7e
Sebastian Osiński <seb.osinski@gmail.com>
2015-12-16 23:59:31 +0100

    Remove Change and TotalChange structs. Add Pair struct (it's like a tuple but can be used as key in dictionary)

--- AdventOfCode/Day 13/day_13.swift
+++ AdventOfCode/Day 13/day_13.swift
@@ -8,75 +8,50 @@
 
 import Foundation
 
-struct Change: CustomStringConvertible {
+struct Pair: Hashable {
     
-    let person: String
-    let neighbor: String
-    let value: Int
-    
-    init(line: String) {
-        let components = line.componentsSeparatedByString(" ")
-        
-        person = components[0]
-        
-        let neighborComponent = components[components.count - 1]
-        neighbor = neighborComponent.substringToIndex(neighborComponent.endIndex.predecessor())
-        
-        value = (components[2] == "gain" ? 1 : -1) * Int(components[3])!
-    }
-    
-    var description: String {
-        return "\(person) --[\(value)]--> \(neighbor)"
-    }
-}
-
-struct TotalChange: CustomStringConvertible, Hashable {
-    
-    let firstPerson: String
-    let secondPerson: String
-    let value: Int
-    
-    init?(change1: Change, change2: Change) {
-        guard change1.person == change2.neighbor && change2.person == change1.neighbor else {
-            return nil
-        }
-        
-        firstPerson = change1.person
-        secondPerson = change2.person
-        value = change1.value + change2.value
-    }
-    
-    var description: String {
-        return "\(firstPerson) <--[\(value)]--> \(secondPerson)"
-    }
+    let personA: String
+    let personB: String
     
     var hashValue: Int {
-        return firstPerson.hashValue &+ secondPerson.hashValue &+ value.hashValue
+        return personA.hashValue &+ personB.hashValue
     }
-
 }
 
-func ==(lhs: TotalChange, rhs: TotalChange) -> Bool {
-    return ((lhs.firstPerson == rhs.firstPerson && lhs.secondPerson == rhs.secondPerson) ||
-            (lhs.firstPerson == rhs.secondPerson && lhs.secondPerson == rhs.firstPerson)) &&
-            lhs.value == rhs.value
+func ==(lhs: Pair, rhs: Pair) -> Bool {
+    return ((lhs.personA == rhs.personA && lhs.personB == rhs.personB) ||
+        (lhs.personA == rhs.personB && lhs.personB == rhs.personA))
 }
 
-func day13() {
+private func getChanges() -> [Pair: Int] {
     let inputPath = NSBundle.mainBundle().pathForResource("day13_input", ofType: nil)
     let lines = try! String(contentsOfFile: inputPath!).componentsSeparatedByString("\n")
     
-    let changes = lines.map { Change(line: $0) }
+    var changes = [Pair: Int]()
     
-    var totalChanges = Set<TotalChange>()
-    
-    for (index1, change1) in changes.enumerate() {
-        for (index2, change2) in changes.enumerate() where index2 != index1 {
-            if let totalChange = TotalChange(change1: change1, change2: change2) {
-                totalChanges.insert(totalChange)
-            }
+    for line in lines {
+        let components = line.componentsSeparatedByString(" ")
+        let personA = components[0]
+        
+        let personBComponent = components[components.count - 1]
+        let personB = personBComponent.substringToIndex(personBComponent.endIndex.predecessor())
+        
+        let value = (components[2] == "gain" ? 1 : -1) * Int(components[3])!
+        
+        let pair = Pair(personA: personA, personB: personB)
+        
+        if let currentValue = changes[pair] {
+            changes[pair] = currentValue + value
+        } else {
+            changes[pair] = value
         }
     }
-    print(totalChanges.count)
-    totalChanges.forEach { print($0) }
+    
+    return changes
+}
+
+func day13() {
+    let changes = getChanges()
+    print(changes)
+    
 }
